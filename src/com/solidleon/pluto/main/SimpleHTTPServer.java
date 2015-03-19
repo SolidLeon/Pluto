a2d544cea7ec9872354761096b7c5c4e10eff881
SolidLeon <markusmannel@gmail.com>
2015-03-19 20:33:13 +0100

    Server port is now configurable

--- src/com/solidleon/pluto/main/SimpleHTTPServer.java
+++ src/com/solidleon/pluto/main/SimpleHTTPServer.java
@@ -14,53 +14,67 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimpleHTTPServer {
+public class SimpleHTTPServer implements Runnable {
+
+    private int port = 2345;
 
 	public static void main(String[] args) {
+        int port = 2345;
 
-		Thread th = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try (ServerSocket server = new ServerSocket(2345)) {
-					System.out.println("SERVER BOUND TO 2345");
-					while (true) {
-						try (Socket socket = server.accept()) {
-							
-							System.out.println("READ REQUEST");
-							BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-							String line = in.readLine();
-							System.out.println(line);
-						
-							String requestFile = line.substring("GET ".length(), line.length() - " HTTP/1.1".length());
-							
-							System.out.println("REQUEST FILE:  '" + requestFile + "'");
-							try (BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
-								File file = new File("C:\\DEPLOYMENT\\public\\" + requestFile);
-								if (file.exists()) {
-									out.write(	("HTTP/1.1 200 OK \r\n"+
-												"Content-Type: text/plain\r\n"+
-												"Content-Length: "+file.length()+"\r\n"+
-												"Connection: close\r\n\r\n")
-											.getBytes());
-									Files.copy(file.toPath(), out);
-								} else {
-									String msg = "File not found";
-									out.write(	("HTTP/1.1 404 Not Found \r\n"+
-												"Content-Type: text/plain\r\n"+
-												"Content-Length: "+msg.length()+"\r\n"+
-												"Connection: close\r\n\r\n")
-											.getBytes());
-									out.write(msg.getBytes());
-								}
-							}
-						}
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-		th.start();
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (Exception ex) {
+            System.err.println("Error parsing command line");
+            System.err.println("USAGE: java -jar Pluto.jar <port>");
+        }
+
+        new Thread(new SimpleHTTPServer(port)).start();
 	}
+
+    public SimpleHTTPServer(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            System.out.println("SERVER BOUND TO " + port);
+            while (true) {
+                try (Socket socket = server.accept()) {
+
+                    System.out.println("READ REQUEST");
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String line = in.readLine();
+                    System.out.println(line);
+
+                    String requestFile = line.substring("GET ".length(), line.length() - " HTTP/1.1".length());
+
+                    System.out.println("REQUEST FILE:  '" + requestFile + "'");
+                    try (BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
+                        File file = new File("C:\\DEPLOYMENT\\public\\" + requestFile);
+                        if (file.exists()) {
+                            out.write(	("HTTP/1.1 200 OK \r\n"+
+                                        "Content-Type: text/plain\r\n"+
+                                        "Content-Length: "+file.length()+"\r\n"+
+                                        "Connection: close\r\n\r\n")
+                                    .getBytes());
+                            Files.copy(file.toPath(), out);
+                        } else {
+                            String msg = "File not found";
+                            out.write(	("HTTP/1.1 404 Not Found \r\n"+
+                                        "Content-Type: text/plain\r\n"+
+                                        "Content-Length: "+msg.length()+"\r\n"+
+                                        "Connection: close\r\n\r\n")
+                                    .getBytes());
+                            out.write(msg.getBytes());
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
