701f888747ac357d5eee54907550328c294ecd98
SolidLeon <markusmannel@gmail.com>
2015-03-20 09:37:58 +0100

    Fixed imports

--- src/com/solidleon/pluto/main/SimpleHTTPServer.java
+++ src/com/solidleon/pluto/main/SimpleHTTPServer.java
@@ -1,6 +1,5 @@
 package com.solidleon.pluto.main; 
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,11 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SimpleHTTPServer implements Runnable {
 
